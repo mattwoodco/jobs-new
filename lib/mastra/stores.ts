11cc71e0b75f1b0ceed6f7346d39926415f65a3a
Clark Kent <man@dailybugle.com>
2025-10-04 23:21:31 -0500

    workign

--- lib/mastra/stores.ts
+++ lib/mastra/stores.ts
@@ -1,7 +1,6 @@
 import { PostgresStore } from "@mastra/pg";
 
 export const storage = new PostgresStore({
-  connectionString:
-    (process.env.JOBS_CHAT_DATABASE_URL ||
-      process.env.CHAT_MEMORY_DATABASE_URL) as string,
+  connectionString: (process.env.JOBS_CHAT_DATABASE_URL ||
+    process.env.CHAT_MEMORY_DATABASE_URL) as string,
 });
